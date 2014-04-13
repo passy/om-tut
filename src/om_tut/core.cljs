2e938301309140a638cab03fd208366fc7023a7e
Pascal Hartig <passy@twitter.com>
2014-04-13 20:04:59 +0100

    WIP: Registry part of the tutorial

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -1,92 +1,59 @@
 (ns om-tut.core
-  (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [cljs.core.async :refer [put! chan <!]]
-            [clojure.data :as data]
             [clojure.string :as string]))
 
 (enable-console-print!)
 
-(defn parse-contact [contact-str]
-  (let [[first middle last :as parts] (string/split contact-str #"\s+")
-        [first last middle] (if (nil? last) [first middle] [first last middle])
-        middle (when middle (string/replace middle "." ""))
-        c (if middle (count middle) 0)]
-    (when (>= (count parts) 2)
-      (cond-> {:first first :last last}
-        (== c 1) (assoc :middle-initial middle)
-        (>= c 2) (assoc :middle middle)))))
-
 (def app-state
   (atom
-    {:contacts
-     [{:first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
-      {:first "Alyssa" :middle-initial "P" :last "Hacker" :email "aphacker@mit.edu"}
-      {:first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
-      {:first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
-      {:first "Cy" :middle-initial "D" :last "Effect" :email "bugs@mit.edu"}
-      {:first "Lem" :middle-initial "E" :last "Tweakit" :email "morebugs@mit.edu"}]}))
+   {:people
+     [{:type :student :first "Ben" :last "Bitdiddle" :email "benb@mit.edu"}
+      {:type :student :first "Alyssa" :middle-initial "P" :last "Hacker"
+       :email "aphacker@mit.edu"}
+      {:type :professor :first "Gerald" :middle "Jay" :last "Sussman"
+       :email "metacirc@mit.edu" :classes [:6001 :6946]}
+      {:type :student :first "Eva" :middle "Lu" :last "Ator" :email "eval@mit.edu"}
+      {:type :student :first "Louis" :last "Reasoner" :email "prolog@mit.edu"}
+      {:type :professor :first "Hal" :last "Abelson" :email "evalapply@mit.edu"
+       :classes [:6001]}]
+     :classes
+     {:6001 "The Structure and Interpretation of Computer Programs"
+      :6946 "The Structure and Interpretation of Classical Mechanics"
+      :1806 "Linear Algebra"}}))
 
 (defn middle-name [{:keys [middle middle-initial]}]
   (cond
-   middle (str " " middle)
-   middle-initial (str " " middle-initial ".")))
+    middle (str " " middle)
+    middle-initial (str " " middle-initial ".")))
 
-(defn display-name [{:keys [first last] :as contact}]
+(defn display-name [{:keys [first last] as contact}]
   (str last ", " first (middle-name contact)))
 
-(defn contact-view [contact owner]
+(defmulti entry-view (fn [person _] (:type person)))
+
+(defmethod entry-view :student
+  [person owner] (student-view person owner))
+
+(defmethod entry-view :professor
+  [person owner] (professor-view person owner))
+
+(defn people [app]
+  (->> (:people app)
+    (mapv (fn [x]
+            (if (:classes x)
+              (update-in x [:classes]
+                (fn [cs] (mapv (:classes app) cs)))
+             x)))))
+
+(defn registry-view [app owner]
   (reify
     om/IRenderState
-    (render-state [this {:keys [delete]}]
-     (dom/li nil
-       (dom/span nil (display-name contact))
-       (dom/button #js {:onClick (fn [e] (put! delete (deref contact)))} "Delete")))))
-
-(defn handle-change [e owner {:keys [text]}]
-  (let [value (.. e -target -value)]
-    (if-not (re-find #"[0-9]" value)
-      (om/set-state! owner :text value)
-      (om/set-state! owner :text text))))
-
-(defn contacts-view [app owner]
-  (reify
-    om/IInitState
-    (init-state [_]
-      {:delete (chan)
-       :text ""})
-    om/IWillMount
-    (will-mount [_]
-      (let [delete (om/get-state owner :delete)]
-        (go (loop []
-          (let [contact (<! delete)]
-            (om/transact! app :contacts
-              (fn [xs] (vec (remove #(= contact %) xs))))
-            (recur))))))
-    om/IRenderState
-    (render-state [this state]
+    (render-state [_ state]
       (dom/div nil
-        (dom/h2 nil "Contact list")
+        (dom/h2 nil "Registry")
         (apply dom/ul nil
-          (om/build-all contact-view (:contacts app)
-            {:init-state state}))
-        (dom/div nil
-          (dom/input #js {:ref "new-contact"
-                          :placeholder "New Contact"
-                          :value (:text state)
-                          :onChange #(handle-change % owner state)})
-          (dom/button #js {:onClick #(add-contact app owner)} "Add"))))))
+          (om/build-all entry-view (people app)))))))
 
-(defn add-contact [app owner]
-  (let [input (om/get-node owner "new-contact")
-        new-contact (-> input
-                        .-value
-                        parse-contact)]
-    (when new-contact
-      (om/transact! app :contacts #(conj % new-contact))
-      (set! (.-value input) ""))))
-
-
-(om/root contacts-view app-state
-  {:target (. js/document (getElementById "contacts"))})
+(om/root registry-view app-state
+  {:target (. js/document (getElementById "registry"))})
